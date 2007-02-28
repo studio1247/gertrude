f173e3ac5d7ce9fedaea30701632993261befc6f
Bertrand Songis <bsongis@gmail.com>
2007-02-28 13:19:46 +0000

    Database creation bugs

---
+++ constants.py
@@ -0,0 +1,26 @@
+# -*- coding: cp1252 -*-
+
+##    This file is part of Gertrude.
+##
+##    Gertrude is free software; you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation; either version 2 of the License, or
+##    (at your option) any later version.
+##
+##    Gertrude is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with Gertrude; if not, write to the Free Software
+##    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+VERSION = 2
+
+PROFIL_INSCRIPTIONS = 1
+PROFIL_TRESORIER = 2
+PROFIL_BUREAU = 4
+PROFIL_SAISIE_PRESENCES = 8
+PROFIL_ADMIN = 16
+PROFIL_ALL = PROFIL_ADMIN + PROFIL_INSCRIPTIONS + PROFIL_TRESORIER + PROFIL_BUREAU + PROFIL_SAISIE_PRESENCES
