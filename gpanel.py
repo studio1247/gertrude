9d9dbbbfa65721765fc56e3d7d580fd6e7ce6307
Bertrand Songis <bsongis@gmail.com>
2007-09-25 07:30:09 +0000

    new gpanel (GetStaticText) was not working under windows

--- gpanel.py
+++ gpanel.py
@@ -1,12 +1,15 @@
-import wx
-import wx.lib.stattext
+import sys
+import wx, wx.lib.stattext
 
 class GPanel(wx.Panel):
     def __init__(self, parent, title):
         wx.Panel.__init__(self, parent, style=wx.LB_DEFAULT)
 	self.sizer = wx.BoxSizer(wx.VERTICAL)
 	sizer = wx.BoxSizer(wx.HORIZONTAL)
-        st = wx.lib.stattext.GenStaticText(self, -1, title, size=(-1, 24), style=wx.BORDER_SUNKEN|wx.ST_NO_AUTORESIZE)
+	if sys.platform == 'win32':
+            st = wx.StaticText(self, -1, title, size=(-1, 24), style=wx.BORDER_SUNKEN|wx.ST_NO_AUTORESIZE)
+        else:    
+            st = wx.lib.stattext.GenStaticText(self, -1, title, size=(-1, 24), style=wx.BORDER_SUNKEN|wx.ST_NO_AUTORESIZE)
         st.SetFont(wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD))
         st.SetBackgroundColour(wx.Colour(10, 36, 106))
         st.SetBackgroundStyle(wx.BG_STYLE_COLOUR)
