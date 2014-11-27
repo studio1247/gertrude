7ba3d1007caae5be06febb561dc467fc89bd4b3c
bsongis <bsongis@gmail.com>
2014-11-27 07:37:39 +0100

    Correction script insertion automatique de tarifs

--- tools/tarifsinsert.py
+++ tools/tarifsinsert.py
@@ -31,24 +31,30 @@ formule_taux_horaire = []
 def add_cell(condition, value):
     global formule_taux_horaire
     formule_taux_horaire.append((condition, value))
+
+def et(cond1, cond2):
+    if cond1 == "":
+        return cond2
+    else:
+        return cond1 + " et " + cond2
     
 def add_row(condition, row):
-    add_cell(condition + " et heures_hebdomadaires<20", row[0])
-    add_cell(condition + " et heures_hebdomadaires<25", row[1])
-    add_cell(condition + " et heures_hebdomadaires<30", row[2])
-    add_cell(condition + " et heures_hebdomadaires<35", row[3])
-    add_cell(condition + " et heures_hebdomadaires<40", row[4])
+    add_cell(et(condition, "heures<20"), row[0])
+    add_cell(et(condition, "heures<25"), row[1])
+    add_cell(et(condition, "heures<30"), row[2])
+    add_cell(et(condition, "heures<35"), row[3])
+    add_cell(et(condition, "heures<40"), row[4])
     add_cell(condition, row[5])
 
 def add_table(condition_enfants, seuils, max):
-    add_row(condition_enfants + " et revenus<%f" % seuils[0], [max-1.0-0.25*i for i in range(6)])
-    add_row(condition_enfants + " et revenus<%f" % seuils[1], [max-0.5-0.25*i for i in range(6)])
+    add_row(et(condition_enfants, "revenus<%d" % seuils[0]), [max-1.0-0.25*i for i in range(6)])
+    add_row(et(condition_enfants, "revenus<%d" % seuils[1]), [max-0.5-0.25*i for i in range(6)])
     add_row(condition_enfants, [max-0.25*i for i in range(6)])
 
 add_table("enfants>3", (2879.0, 6398.0), 8.25)
 add_table("enfants=3", (26043.0, 57873.0), 8.5)
 add_table("enfants=2", (23164.0, 51475.0), 8.75)
-add_table("enfants=1", (20285.0, 45077.0), 9.0)
+add_table("", (20285.0, 45077.0), 9.0)
 
 cur.execute('UPDATE creche SET formule_taux_horaire=?', (str(formule_taux_horaire),))
         
