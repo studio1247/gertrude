147fb83fa7fc7b8a527c8791c2520583751bcaeb
Bertrand Songis <bsongis@gmail.com>
2015-11-24 13:52:52 +0100

    Version 1.02a

--- version.py
+++ version.py
@@ -15,4 +15,4 @@
 ##    You should have received a copy of the GNU General Public License
 ##    along with Gertrude; if not, see <http://www.gnu.org/licenses/>.
 
-VERSION = '1.02g'
+VERSION = '1.01a'
