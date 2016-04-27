42bfa9fd2b09c279d665c514b6626184e63fc931
Bertrand Songis <bsongis@gmail.com>
2016-04-27 20:56:19 +0200

    Version 1.05m

--- version.py
+++ version.py
@@ -15,4 +15,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Gertrude; if not, see <http://www.gnu.org/licenses/>.
 
-VERSION = '1.05l'
+VERSION = '1.05m'
