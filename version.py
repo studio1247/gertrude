416697c132c3babccaabd87624d08d8a18da6914
Bertrand Songis <bsongis@gmail.com>
2017-03-05 17:40:08 +0100

    Version 1.09l

--- version.py
+++ version.py
@@ -15,4 +15,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Gertrude; if not, see <http://www.gnu.org/licenses/>.
 
-VERSION = '1.09k'
+VERSION = '1.09l'
