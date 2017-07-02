851f80b98c775f3fb00348dc9be80571c6721424
Bertrand Songis <bsongis@gmail.com>
2017-07-02 19:51:16 +0200

    Ajustement erreurs de saisie tablette

--- tablette.py
+++ tablette.py
@@ -149,7 +149,7 @@ def sync_tablette_lines(lines, tz=None):
                     last = array[idx][date][-1]
                     if not last.arrivee or not last.depart:
                         last.depart = depart
-                    elif last.arrivee and last.depart and depart - last.depart < (creche.granularite / BASE_GRANULARITY):
+                    elif last.arrivee and last.depart and depart - last.depart < 2 * (creche.granularite / BASE_GRANULARITY):
                         last.depart = depart
                     else:
                         array[idx][date].append(PeriodePresence(date, None, depart))
