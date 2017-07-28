ea1137a23a189ad632ad56310ea32b8123881896
Bertrand Songis <bsongis@gmail.com>
2017-07-28 14:35:13 +0200

    Python requests added to MacOSX package

--- package.py
+++ package.py
@@ -76,7 +76,7 @@ def main():
                    'arch': 'i386',
                    'iconfile': 'bitmaps_dist/gertrude.icns',
                    'argv_emulation': True,
-                   'includes': ['bcrypt', "_cffi_backend"]
+                   'includes': ['bcrypt', "_cffi_backend", "requests"]
                    }
         setup(
             name="Gertrude",
