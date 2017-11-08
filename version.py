15e20d367f471686235ac5fa7ad0f320768cc504
Bertrand Songis <bsongis@gmail.com>
2017-11-08 23:09:50 +0100

    Version 1.20.06

--- version.py
+++ version.py
@@ -15,4 +15,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Gertrude; if not, see <http://www.gnu.org/licenses/>.
 
-VERSION = '1.20.05'
+VERSION = '1.20.06'
