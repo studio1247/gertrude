518ca3a815f26508c639b370a2a52d6da033260e
Bertrand Songis <bsongis@gmail.com>
2017-12-01 00:31:52 +0100

    Correction

--- generation/contrat_accueil.py
+++ generation/contrat_accueil.py
@@ -144,8 +144,8 @@ class DocumentAccueilModifications(object):
         for jour in range(inscription.duree_reference):
             jour_reference = inscription.get_day_from_index(jour)
             debut, fin = jour_reference.GetPlageHoraire()
-            fields.append(('heure-debut[%d]' % jour, GetHeureString(5*debut)))
-            fields.append(('heure-fin[%d]' % jour, GetHeureString(5*fin)))
+            fields.append(('heure-debut[%d]' % jour, GetHeureString(debut/12 if debut else None)))
+            fields.append(('heure-fin[%d]' % jour, GetHeureString(fin/12 if fin else None)))
             fields.append(('heures-jour[%d]' % jour, GetHeureString(jour_reference.get_duration())))
 
         for key in database.creche.activites:
