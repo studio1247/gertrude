0f3b77bc495c9bcc55d22ddb12cb6590dbec1f8a
Bertrand Songis <bsongis@gmail.com>
2018-01-05 00:28:59 +0100

    Version 1.21.4

--- version.py
+++ version.py
@@ -15,4 +15,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Gertrude; if not, see <http://www.gnu.org/licenses/>.
 
-VERSION = '1.21.3'
+VERSION = '1.21.4'
