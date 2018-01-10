8dfebacb421870501248f64e3b8e554408e89383
Bertrand Songis <bsongis@gmail.com>
2018-01-10 08:54:44 +0100

    Version 1.21.6

--- version.py
+++ version.py
@@ -15,4 +15,4 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Gertrude; if not, see <http://www.gnu.org/licenses/>.
 
-VERSION = '1.21.5'
+VERSION = '1.21.6'
